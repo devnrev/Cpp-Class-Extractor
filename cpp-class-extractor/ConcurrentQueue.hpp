70404f885bd718f0d219408c64dc400119545fc7
Axel <axeleng90@googlemail.com>
2012-05-22 19:53:27 +0200

    little changes

---
+++ cpp-class-extractor/ConcurrentQueue.hpp
@@ -0,0 +1,17 @@
+//
+//  Created by Axel on 05.05.12.
+//
+
+
+#ifndef __ConcurrentQueue_H_
+#define __ConcurrentQueue_H_
+template<
+class ConcurrentQueue {
+public:
+
+protected:
+
+private:
+};
+
+#endif //__ConcurrentQueue_H_
